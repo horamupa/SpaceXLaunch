45c27fe5eb0839fbdf4dd2035e0d600a3d7e2d0e
horamupa <horamupa@gmail.com>
2022-08-15 19:01:54 +0300

    Initial Commit

---
+++ SpaceXLaunch/SpaceXLaunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXLaunchApp.swift
+//  SpaceXLaunch
+//
+//  Created by MM on 15.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXLaunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
